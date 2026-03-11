c40321a59d9b6ab3729f8d10528469ebc2a28ff9
ivanTrotta <136804202+ivanTrotta@users.noreply.github.com>
2026-03-11 15:27:59 +0100

    Remove Twitch followers fetch functionality
    Removed fetch call to retrieve Twitch followers.

--- js/script.js
+++ js/script.js
@@ -1,8 +1,1 @@
-fetch("https://decapi.me/twitch/followers/ivantrotta571")
-.then(response => response.text())
-.then(data => {
 
-document.getElementById("followers").innerText =
-"Follower Twitch: " + data;
-
-});
