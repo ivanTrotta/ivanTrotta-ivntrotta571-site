c9109bb0108e070e010706f1bebf38b6ba928a48
ivanTrotta <136804202+ivanTrotta@users.noreply.github.com>
2026-03-25 09:33:42 +0100

    Add live status indicator functionality

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
+const status = document.getElementById("live-status");
 
+// Cambio manualmente
+let isLive = false; // true = live, false = offline
+
+if(isLive){
+    status.textContent = "🟢 LIVE ORA";
+    status.classList.add("live");
+}else{
+    status.textContent = "🔴 Offline";
+    status.classList.add("offline");
+}
